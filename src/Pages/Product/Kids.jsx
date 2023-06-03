2b4d42fe4fe46f67d2a45d7b42b9eb38b1553a0b
hunbp75 <hunbp75@gmail.com>
2023-06-03 18:21:49 +0200

    Fixed the Kids.jsx

---
+++ src/Pages/Product/Kids.jsx
@@ -0,0 +1,29 @@
+const Kids = () => {
+  return (
+    <div>
+      <h1>Gyermek Szolgáltatások</h1>
+      <br></br>
+      <br></br>
+      <div className="pedicure">
+        <h3>
+          <u>Szolgáltatások</u>
+        </h3>
+        <span>Kid Fresh Manikűr/ Spa - 13000 Ft</span>
+        <br></br>
+        <span>Polírcsere kézen/lábon - 5000 Ft</span>
+        <br></br>
+        <span>levegőre kötő lakk hozzáadása kézre/lábra - 14000 Ft</span>
+        <br></br>
+        <span>Kombinált gyerek manikűr + pedikűr - 14500 Ft</span>
+        <br></br>
+        <span>Kid Fresh Pedikűr/ Spa - 12000 Ft</span>
+        <br></br>
+        <span>Porcelán műköröm újraépítése - 19000 Ft</span>
+        <br></br>
+      </div>
+      <br></br> <br></br>
+    </div>
+  );
+};
+
+export default Kids;
