205b47dbcc75bc96be28dc96495a3da96d42d41e
hunbp75 <hunbp75@gmail.com>
2023-06-03 14:43:08 +0200

    finished the Manicure modul

--- src/Pages/Product/Manicure.jsx
+++ src/Pages/Product/Manicure.jsx
@@ -9,6 +9,8 @@ const Manicure = () => {
         Bőrünket paraffinos kézápolással selymessé varázsolhatjuk.
       </p>
       <h3>
+        {" "}
+        <br></br>
         <u>GÉPI MANIKŰR</u>
       </h3>
       <p>7000 Ft</p>
@@ -21,10 +23,11 @@ const Manicure = () => {
         körmöd felületét kérheted mattra vagy fényesre polírozva. A szolgáltatás
         befejezése a bőr hatékony ápolása
       </p>
+      <br></br>
       <h3>
         <u>JAPÁN MANIKŰR</u>
       </h3>
-      <p>10000 Ft</p>
+      <p>Ár: 10000 Ft</p>
       <p>Szolgáltatás várható ideje: 1 óra</p>
       <p>Szolgáltatás tartalma: Japán manikűr</p>
       <p>
@@ -33,11 +36,11 @@ const Manicure = () => {
         táplálja, ápolja, rugalmassá teszi a természetes körömlemezt és
         visszaadja annak természetes fényét.
       </p>
-
+      <br></br>
       <h3>
         <u>KLASSZIKUS MANIKŰR</u>
       </h3>
-      <p>7000 Ft</p>
+      <p>Ár: 7000 Ft</p>
       <p>Szolgáltatás várható ideje: 30 perc</p>
       <p>Szolgáltatás tartalma: Gépi manikűr</p>
       <p>
@@ -47,6 +50,58 @@ const Manicure = () => {
         körmöd felületét kérheted mattra vagy fényesre polírozva. A szolgáltatás
         befejezése a bőr hatékony ápolása
       </p>
+      <br></br>
+      <h1>Gél Lakk szolgáltatások</h1>
+      <br></br>
+      <h3>
+        <u>GÉL LAKK - EGYSZÍNŰ</u>
+      </h3>
+      <p>Ár: 9500 Ft</p>
+      <p>Szolgáltatás várható időtartama: 1 óra</p>
+      <p>
+        Szolgáltatás tartalma: Gépi manikűr, Előző gél lakk eltávolítása,
+        Egyszínű erősített gél lakkozás. Gépi manikűr és az előző gél lakk
+        eltávolítását követően egy erősített gél lakkozást készítem neked. A
+        körmöd lehet egyszínű vagy minden körmöd más színű. A szolgáltatásban
+        válaszd gél lakk prémiumszolgáltatatásunkat.
+      </p>
+      <br></br>
+      <h3>
+        <u>GÉL LAKK - PRÉMIUM</u>
+      </h3>
+      <p>Ár: 10500 Ft</p>
+      <p>Szolgáltatás várható időtartama: 1,5 óra</p>
+      <p>
+        Szolgáltatás tartalma: Gépi manikűr, Előző gél lakk eltávolítása,
+        Erősített gél lakkozás (egyszínű, Ombre, Francia, Babyboomer), 2
+        díszítés
+      </p>
+      <p>
+        Gépi manikűr és az előző gél lakk eltávolítását követően egy erősített
+        gél lakkozást készítem neked. Prémium szolgáltatásunk során választhatod
+        az egyszínű, francia, ombre vagy akár babyboomer díszítéseket is. A
+        szolgáltatásba foglalt 2 díszítésed lehet kissé extrémebb, a
+        szolgáltatás időtartományán belül.
+      </p>
+      <br></br>
+      <h3>
+        <u>GÉL LAKK VÉGLEGES ELTÁVOLÍTÁS</u>
+      </h3>
+      <p>Ár: 7000 Ft</p>
+      <p>Szolgáltatás várható időtartama: 45 perc</p>
+      <p>Szolgáltatás tartalma: Gépi manikűr, Gél lakk eltávolítás.</p>
+      <p>
+        A gél lakk végleges eltávolítása csiszológép segítségével és oldással
+        történik. A szolgáltatás része a gépi manikűr és a körmök fényének
+        visszaadása polírozással. A gél lakk végleges eltávolítása csiszológép
+        segítségével és oldással történik. A szolgáltatás része a gépi manikűr
+        és a körmök fényének visszaadása polírozással.A gél lakk végleges
+        eltávolítása csiszológép segítségével és oldással történik. A
+        szolgáltatás része a gépi manikűr és a körmök fényének visszaadása
+        polírozással.
+      </p>
+      <br></br>
+      <br></br>
     </div>
   );
 };
