1e4a2f0be750e8ed39a96ac0afc34c09b14d7db2
hunbp75 <hunbp75@gmail.com>
2023-06-03 18:09:55 +0200

    fixed the Nails.jsx

---
+++ src/Pages/Product/Nails.jsx
@@ -0,0 +1,88 @@
+const Nails = () => {
+  return (
+    <div>
+      <h1>Műköröm Szolgáltatások</h1>
+      <div className="pedicureMain">
+        <p>
+          Tartós, trendhez hű és személyiségéhez illő műkörmökkel és körmei
+          díszítésével, ápolásával járulok hozzá kényelméhez. Négy, vagy akár 7
+          hétig tartó, strapabíró körmöket építek, melyeket ezután igény szerint
+          töltéskor akár más dizájnra vagy fazonra változtathatunk.
+        </p>
+      </div>
+      <br></br>
+      <br></br>
+      <div className="pedicure">
+        <h3>
+          <u>Szolgáltatások</u>
+        </h3>
+        <span>Gél lakozás nélkül - Ft</span>
+        <br></br>
+        <span>Zselés műköröm töltés - 15000 Ft</span>
+        <br></br>
+        <span>Zselés műköröm újraépítése - 18000 Ft</span>
+        <br></br>
+        <span>Porcelán műköröm építés - 17000 Ft</span>
+        <br></br>
+        <span>Porcelán műköröm töltés - 17000 Ft</span>
+        <br></br>
+        <span>Porcelán műköröm újraépítése - 19000 Ft</span>
+        <br></br>
+        <span>Műköröm építés egyszinű - 14000 Ft</span>
+        <br></br>
+        <span>Műköröm építés prémium + gél lakk - 18000 Ft</span>
+        <br></br>
+        <span>Műköröm építés XL - 20000 Ft</span>
+        <br></br>
+        <span>Műköröm építés extra forma - 29800 Ft</span>
+        <br></br>
+        <span>Műköröm újraépítés egyszínű - 22000 Ft</span>
+        <br></br>
+        <span>Műköröm újtaépítés prémium + gél lakk - 23000 Ft</span>
+        <br></br>
+        <span>Műköröm újraépítés XL - 29800 Ft</span>
+        <br></br>
+        <span>Műköröm újraépítés - extrém forma - 32400 Ft</span>
+        <br></br>
+        <span>Műköröm töltés egyszínű - 15000 Ft</span>
+        <br></br>
+        <span>Műköröm töltés prémium + gél lakk - 19500 Ft</span>
+        <br></br>
+        <span>Műköröm töltés XL - 22800 Ft</span>
+        <br></br>
+        <span>Pótlás más szolgáltatás mellé - 2500 Ft/db</span>
+        <br></br>
+        <span>
+          Műköröm/Gél lakk eltávolítása más szolgáltatás előtt - 5000 Ft
+        </span>
+        <br></br>
+        <span>Műköröm végleges eltávolítása - 8500 Ft</span>
+        <br></br>
+      </div>
+      <br></br>
+      <br></br>
+      <div className="pedicure">
+        <h3>
+          <u>Díszítések</u>
+        </h3>
+        <span>swarovski formakő - 600 Ft/db</span>
+        <br></br>
+        <span>swarovski starssz - 100 Ft/db</span>
+        <br></br>
+        <span>swarovski Pixie kisebb díszítés - 700 Ft/db</span>
+        <br></br>
+        <span>swarovski Pixie teljes körömre (normál hossz) - 1800 Ft/db</span>
+        <br></br>
+        <span>swarovski Pixie teljes körömre (XL) - 2500 Ft/db</span>
+        <br></br>
+        <span>Plusz díszítés - 400 Ft/db</span>
+        <br></br>
+        <span>Extra díszítés - 1000 - 1500 Ft/db</span>
+        <br></br>
+      </div>
+      <br></br> <br></br>
+    </div>
+  );
+};
+
+export default Nails;
