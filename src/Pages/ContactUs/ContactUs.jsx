504bdbcdc4d0ecfecd8bfe4e2f187c84dbd706ec
hunbp75 <hunbp75@gmail.com>
2023-06-04 09:16:35 +0200

    working on the ContactUs page

--- src/Pages/ContactUs/ContactUs.jsx
+++ src/Pages/ContactUs/ContactUs.jsx
@@ -1,13 +1,23 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import "./contactus.css";
 
 const ContactUs = () => {
   return (
     <div className="contactus_main">
       <h1>LAKK Szépségszalon</h1>
-      <h3>cím: 2000, Szentendre Stromfeld utca 1/a</h3>
-      <h3>email: lakkszepsegszalon@gmail.com</h3>
-      <h3>telefon: +36 20 474-9390</h3>
+      <h3>Monza Hungary Kft.</h3>
+      <h3>2000, Szentendre Stromfeld utca 1/a</h3>
+      <h3>
+        <FontAwesomeIcon icon={faEnvelope} /> lakkszepsegszalon@gmail.com
+      </h3>
+      <h3>
+        <FontAwesomeIcon icon={faPhone} /> +36 20 474-9390
+      </h3>
+      <h3>
+        <FontAwesomeIcon icon={faPhone} /> +36 20 599-2525
+      </h3>
     </div>
   );
 };
