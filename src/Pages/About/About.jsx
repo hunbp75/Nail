7016a56c92c7c95d1642594ea9be2f96e3ab07fc
hunbp75 <hunbp75@gmail.com>
2023-06-22 20:44:41 +0200

    fixes colors

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -3,28 +3,33 @@ import "./about.css";
 
 const About = () => {
   return (
-    <div className="container_about">
-      <p>
-        Üdvözöljük a Nails by Vera, a szentendrei körömszalonban, amely büszke
-        arra, hogy a város legjobb manikűr és pedikűr szolgáltatásait kínálja!
-        Profi csapatunk készen áll arra, hogy felejthetetlen élményt nyújtson
-        Önnek.
-      </p>
-      <p>
-        A körömkezelések széles választékát kínáljuk, amelyek garantálják az
-        egészséges és szép körmöket. Akár klasszikus manikűrre, akár gyors
-        körömkezelésre vágyik, akkor is professzionális eredményt érhet el, ha a
-        klasszikus szolgáltatásokat választja.
-      </p>
-      <p>
-        Azért vagyunk itt, hogy segítsünk ötleteket készíteni és kiválasztani az
-        igényeinek leginkább megfelelő megoldást. Jöjjön el és lazítson egy
-        kényelmes és kellemes légkörben, miközben megcsinálja körmeit.
-        Szentendre legjobb körömszalonjában. Engedje meg, hogy elkényeztessük,
-        és segítünk, hogy távozáskor széles mosoly legyen az arcán. Az időpont
-        egyeztetés gyors és egyszerű, ezért ne habozzon kapcsolatba lépni
-        velünk, hogy időpontot foglaljunk még most!
-      </p>
+    <div className="main_aboutUs">
+      <div className="container_about">
+        <p>
+          <div className="text_rainbow">
+            <h1>Nails by Vera</h1>
+          </div>
+          Üdvözöljük a Nails by Vera, a szentendrei körömszalonban, amely büszke
+          arra, hogy a város legjobb manikűr és pedikűr szolgáltatásait kínálja!
+          Profi csapatunk készen áll arra, hogy felejthetetlen élményt nyújtson
+          Önnek.
+        </p>
+        <p>
+          A körömkezelések széles választékát kínáljuk, amelyek garantálják az
+          egészséges és szép körmöket. Akár klasszikus manikűrre, akár gyors
+          körömkezelésre vágyik, akkor is professzionális eredményt érhet el, ha
+          a klasszikus szolgáltatásokat választja.
+        </p>
+        <p>
+          Azért vagyunk itt, hogy segítsünk ötleteket készíteni és kiválasztani
+          az igényeinek leginkább megfelelő megoldást. Jöjjön el és lazítson egy
+          kényelmes és kellemes légkörben, miközben megcsinálja körmeit.
+          Szentendre legjobb körömszalonjában. Engedje meg, hogy elkényeztessük,
+          és segítünk, hogy távozáskor széles mosoly legyen az arcán. Az időpont
+          egyeztetés gyors és egyszerű, ezért ne habozzon kapcsolatba lépni
+          velünk, hogy időpontot foglaljunk még most!
+        </p>
+      </div>
     </div>
   );
 };
